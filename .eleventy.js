16b26b2f7fc841a88a62cce16fab45ca86570d32
Jared Perkins <jared.perkins@hotmail.com>
2026-04-16 11:28:31 -0400

    Installed luxon to help with displaying a date for a post.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
-module.exports = function(eleventyConfig) {
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets");
+
+    eleventyConfig.addFilter("readableDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc'}).toFormat("dd LLL yyyy");
+    });
+
     return {
         dir: {
             input: "src",
